df882d1c4c426fd36a55ccb822bb418ac7249443
jonasbergstrom <jontrian97@gmail.com>
2021-09-02 16:41:07 +0200

    Created the different views for the crypto information

--- components/ListItems.js
+++ components/ListItems.js
@@ -1,12 +1,43 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, Image} from 'react-native'
 
 const ListItems = () => {
     return (
-        <View>
-            <Text>List Item</Text>
-        </View>
+        <TouchableOpacity>
+            <View style={styles.itemWrapper}>
+
+            {/* Left side View */}
+
+            <View style={styles.LeftWrapper}>
+                <Image source={{ uri: "https://upload.wikimedia.org/wikipedia/commons/6/6f/Ethereum-icon-purple.svg" }} style={styles.image}/>
+            {/* Title */}
+            <View style={styles.titleWrapper}>
+                <Text style={styles.title}>Ethereum</Text>
+                <Text style={styles.subtitle}>ETH</Text>
+            </View>
+            </View>
+
+            {/* Right side View */}
+
+            <View style={styles.RightWrapper}>
+                <Text style={styles.title}>Ethereum</Text>
+                <Text style={[styles.subtitle, {color: 'red'}]}>ETH</Text>
+            </View>
+
+
+            </View>
+        </TouchableOpacity>
     )
 }
+
+const styles = StyleSheet.create({
+
+    LeftWrapper: {},
+    titleWrapper: {},
+    title: {},
+    subtitle: {},
+    RightWrapper: {},
+
+})
 
 export default ListItems
